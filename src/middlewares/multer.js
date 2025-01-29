a0111c86a2acc1ea7d6e5f6e6362413e4f4d3685
zero <sanyafullgit@gmail.com>
2025-01-29 16:29:59 +0200

    all tasks done

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,6 +3,8 @@
 import multer from 'multer';
 import { TEMP_UPLOAD_DIR } from '../constants/index.js';
 
+const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, TEMP_UPLOAD_DIR);
@@ -13,4 +15,21 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'),
+      false,
+    );
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+export { upload };
