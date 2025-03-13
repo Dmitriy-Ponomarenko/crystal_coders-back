44962c61c8521286ea18c173a128958520aaf6e9
SvetaSaletska <svitlanagalevich@gmail.com>
2025-03-13 12:03:40 +0200

    Update rateLimit.js

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -3,7 +3,7 @@
 import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 1 * 60 * 1000, 
+  limit: 100, 
   message: 'Too many requests from this IP, please try again later.',
 });
